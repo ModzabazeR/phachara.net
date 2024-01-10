fdd69e754147a3e301c0c3d8a7959fbab74bade0
ModzabazeR <moddaeng.pps@gmail.com>
2024-01-10 17:57:54 +0700

    add comingsoon text move

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,33 @@
-<script>
+<script lang="ts">
+  import { spring } from "svelte/motion";
+
+  let container: HTMLDivElement;
+  let coords = spring(
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      stiffness: 0.1,
+      damping: 0.25,
+    }
+  );
+  let size = spring(1, {
+    stiffness: 0.1,
+    damping: 0.25,
+  });
+
+  const mouseHandler = (e: MouseEvent, scale: number = 1) => {
+    const containerRect = container.getBoundingClientRect();
+    const x = e.clientX;
+    const y = e.clientY;
+    coords.set({
+      x: x - containerRect.width + containerRect.right / 2,
+      y: y - containerRect.height + containerRect.bottom / 2,
+    });
+
+    size.set(scale);
+  };
 </script>
 
 <svelte:head>
@@ -6,24 +35,23 @@
   <meta name="description" content="Phachara Chirapakachote's Portfolio" />
 </svelte:head>
 
-<section>
-  <h1>Coming Soon</h1>
+<section
+  class="h-screen bg-[#1a1a1a] overflow-hidden cursor-none"
+  on:mousemove={mouseHandler}
+  on:mousedown={(e) => {
+    mouseHandler(e, 1.5);
+  }}
+  on:mouseup={(e) => {
+    mouseHandler(e);
+  }}
+  role="presentation"
+>
+  <div bind:this={container} class="w-fit select-none p-8">
+    <h1
+      class="text-4xl font-thin text-[#f5f5f5] font-sans"
+      style="transform: translate({$coords.x}px, {$coords.y}px) scale({$size});"
+    >
+      Coming Soon
+    </h1>
+  </div>
 </section>
-
-<style>
-  section {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    background-color: #1a1a1a;
-  }
-
-  h1 {
-    font-size: 3rem;
-    font-family: "Roboto", sans-serif;
-    font-weight: 300;
-    color: #f5f5f5;
-  }
-</style>
