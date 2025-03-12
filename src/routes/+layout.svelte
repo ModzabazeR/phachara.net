66770a45ebac78d17d8cd01e6fc1bbeb29ae8ffc
ModzabazeR <moddaeng.pps@gmail.com>
2025-03-12 22:44:47 +0700

    init home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,16 @@
-<script>
-	import './styles.css';
+<script lang="ts">
+  import "./styles.css";
+  import "@fontsource/fira-mono";
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/webcam.svg" />
+  <link rel="icon" href="/webcam.svg" />
 </svelte:head>
-	
+
 <div class="app">
-	<main>
-		<slot />
-	</main>
+  <main>
+    {@render children?.()}
+  </main>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,6 @@
 <script lang="ts">
-  import { spring } from "svelte/motion";
-
-  let container: HTMLDivElement;
-  let coords = spring(
-    {
-      x: 0,
-      y: 0,
-    },
-    {
-      stiffness: 0.1,
-      damping: 0.25,
-    }
-  );
-  let size = spring(1, {
-    stiffness: 0.1,
-    damping: 0.25,
-  });
-
-  const mouseHandler = (e: MouseEvent, scale: number = 1) => {
-    const containerRect = container.getBoundingClientRect();
-    const x = e.clientX;
-    const y = e.clientY;
-    coords.set({
-      x: x - containerRect.width + containerRect.right / 2,
-      y: y - containerRect.height + containerRect.bottom / 2,
-    });
-
-    size.set(scale);
-  };
+  import { fade } from "svelte/transition";
+  import Navbar from "../components/Navbar.svelte";
 </script>
 
 <svelte:head>
@@ -35,23 +8,17 @@
   <meta name="description" content="Phachara Chirapakachote's Portfolio" />
 </svelte:head>
 
-<section
-  class="h-screen bg-[#1a1a1a] overflow-hidden cursor-none"
-  on:mousemove={mouseHandler}
-  on:mousedown={(e) => {
-    mouseHandler(e, 1.5);
-  }}
-  on:mouseup={(e) => {
-    mouseHandler(e);
-  }}
-  role="presentation"
->
-  <div bind:this={container} class="w-fit select-none p-8">
-    <h1
-      class="text-4xl font-thin text-[#f5f5f5] font-sans"
-      style="transform: translate({$coords.x}px, {$coords.y}px) scale({$size});"
-    >
-      Coming Soon
+<Navbar />
+<section class="h-dvh flex justify-center items-center">
+  <div
+    transition:fade={{ duration: 800 }}
+    class="flex flex-col gap-y-2 md:gap-y-4 lg:gap-y-6 text-center px-4"
+  >
+    <h1 class="text-2xl md:text-4xl lg:text-6xl font-bold">
+      Phachara Chirapakachote
     </h1>
+    <h4 class="lg:text-lg text-gray-600 dark:text-gray-300 tracking-wide">
+      Full-stack Web Developer | Project Manager | Cybersecurity Enthusiast
+    </h4>
   </div>
 </section>
